0da29cb7d56809b6c6d6824af113d058e1c057bf
snehagarg1 <154874811+snehagarg1@users.noreply.github.com>
2024-01-03 09:32:23 +0000

    arrays

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "hitesh"
+const snapStreak = 100
+
+//console.log(name + snapStreak +" blah blah ");
+
+console.log(`heloo my name is ${name } and my snap is ${snapStreak}`); 
+//use backtick and string interpulation for injecting direct variables
+
+const gameName = new String ("snehablahblah");
+console.log(gameName.__proto__.length);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,12 @@
+//arrays
+
+const myArr = [1,2,3,4,5]
+const myHeroes = ["saktiman ","ironman"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0]);
+
+// array methods 
+
+myArr.push(6)
+myArr.pop()
+console.log(myArr.pop());
